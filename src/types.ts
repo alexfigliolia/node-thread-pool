b511180d1e72c19f16ba5df9e996de8d3b17eee3
Alex Figliolia <alexfigliolia@gmail.com>
2026-09-10 22:10:52 -0700

    More docs (#2)

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,66 @@
 export interface IThreadOptions {
+  /**
+   * maxConcurrency
+   *
+   * The maximum number of concurrent tasks allowed on a given thread
+   *
+   * Defaults to `Infinity`
+   */
   maxConcurrency?: number;
+  /**
+   * workerScript
+   *
+   * The file path or URL to your worker script containing a `ThreadPoolWorker` instance
+   */
   workerScript: string | URL;
+  /**
+   * threadIdleTimeout
+   *
+   * A threshold of milliseconds a thread should wait before shutting down when idle
+   *
+   * Defaults to `2000`
+   */
   threadIdleTimeout?: number;
+  /**
+   * taskTimeoutThreshold
+   *
+   * A timeout for tasks spawned on a thread. Using this threshold, your enqueued task's promise will reject if not complete
+   * within the duration of the threshold
+   *
+   * Defaults to `Infinity`
+   */
   taskTimeoutThreshold?: number;
 }
 
 export interface IThreadPool extends IThreadOptions {
+  /**
+   * lazySpawnThreads
+   *
+   * Whether threads in the pool should spawn as needed based on the pool's task throughput.
+   *
+   * When false, a thread for each of the number of `totalThreads` will be pre-allocated
+   *
+   * Defaults to `true`
+   */
   lazySpawnThreads?: boolean;
+  /**
+   * totalThreads
+   *
+   * Whether threads in the pool should spawn as needed based on the pool's task throughput.
+   *
+   * When false, a thread for each of the number of `totalThreads` will be pre-allocated
+   *
+   * Defaults to the half the number of CPU cores
+   */
   totalThreads?: number;
 }
 
 export interface IThread extends IThreadOptions {
-  workerScript: string | URL;
+  /**
+   * onDestroy
+   *
+   * An optional callback to run when a thread is shut down
+   */
   onDestroy?: () => void;
 }
 
