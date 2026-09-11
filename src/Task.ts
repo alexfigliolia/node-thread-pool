994b36b4fad036f9cbdc2b622642ac97ab6d275c
Alex Figliolia <alexfigliolia@gmail.com>
2026-09-10 21:56:55 -0700

    Refine api and docs

--- src/Task.ts
+++ src/Task.ts
@@ -1,48 +1,56 @@
-import type { Worker } from "node:worker_threads";
-
 import { AutoIncrementingID } from "@figliolia/event-emitter";
 
-import type { WorkerArgs, WorkerError } from "./types";
+import { WorkerResolver, type IWorkerResult } from "./WorkerResolver";
+import { TaskStatus } from "./types";
+import type { Thread } from "./Thread";
 
-export class Task<
-  Args extends Record<string, any>,
-  Result extends Record<string, any>,
-> {
+/**
+ * Task
+ *
+ * A wrapper around a threaded task. With a reference to it,
+ * you can await the result of it's operation or query its status
+ */
+export class Task<Args extends Record<string, any>, Result> {
   public readonly ID: string;
+  public status = TaskStatus.PENDING;
   private static readonly IDs = new AutoIncrementingID();
+  public readonly resolvers: PromiseWithResolvers<Result>;
   private timer: ReturnType<typeof setTimeout> | null = null;
   constructor(
     public readonly args: Args,
     public readonly timeoutThreshold?: number,
   ) {
     this.ID = Task.IDs.get();
+    this.resolvers = Promise.withResolvers();
   }
 
-  public run(worker: Worker) {
+  public run(thread: Thread<Args, Result>) {
     const workerArgs = this.toWorkerArgs(this.args);
-    const { resolve, reject, promise } =
-      Promise.withResolvers<WorkerArgs<Result>>();
-    const onMessage = this.onMessage(workerArgs, resolve);
-    const onError = this.onError(workerArgs, reject);
-    worker.on("message", onMessage);
-    worker.on("error", onError);
+    const onMessage = this.onMessage(this.ID);
+    const onError = this.onError(thread);
+    thread.Worker.on("message", onMessage);
+    thread.Worker.on("error", onError);
     const OFF = () => {
       this.killTimer();
-      worker.off("message", onMessage);
-      worker.off("error", onError);
+      thread.Worker.off("message", onMessage);
+      thread.Worker.off("error", onError);
     };
-    this.configureTimeout(onError);
-    worker.postMessage(workerArgs);
-    return promise.finally(OFF);
+    this.configureTimeout();
+    thread.Worker.postMessage(workerArgs);
+    return this.resolvers.promise.finally(OFF);
   }
 
-  private configureTimeout(onError: (error: WorkerError) => void) {
+  public reject(error: string) {
+    this.resolvers.reject(WorkerResolver.from(this.ID).error(error));
+  }
+
+  private configureTimeout() {
     if (
       typeof this.timeoutThreshold === "number" &&
       isFinite(this.timeoutThreshold)
     ) {
       this.timer = setTimeout(() => {
-        onError(this.toWorkerArgs({ reason: "Task timed out" }));
+        this.reject("Task timed out");
       }, this.timeoutThreshold);
     }
   }
@@ -58,26 +66,26 @@ export class Task<
     return { ...args, __WORKER_POOL_ID__: this.ID };
   }
 
-  private onMessage(
-    workerArgs: WorkerArgs<Args>,
-    resolve: (
-      value: WorkerArgs<Result> | PromiseLike<WorkerArgs<Result>>,
-    ) => void,
-  ) {
-    return (data: WorkerArgs<Result>) => {
-      if (data.__WORKER_POOL_ID__ === workerArgs.__WORKER_POOL_ID__) {
-        resolve(data);
+  private onMessage(ID: string) {
+    return (data: IWorkerResult<Result, Error>) => {
+      if (data.__WORKER_POOL_ID__ === ID) {
+        if ("error" in data) {
+          this.status = TaskStatus.FAILED;
+          this.resolvers.reject(data.error);
+        } else {
+          this.status = TaskStatus.SUCCEEDED;
+          this.resolvers.resolve(data.result);
+        }
       }
     };
   }
 
-  private onError(
-    workerArgs: WorkerArgs<Args>,
-    reject: (reason?: any) => void,
-  ) {
-    return (error: WorkerError) => {
-      if (error.__WORKER_POOL_ID__ === workerArgs.__WORKER_POOL_ID__) {
-        reject(error);
+  private onError(thread: Thread<Args, Result>) {
+    return (error: Error) => {
+      if (!("__WORKER_POOL_ID__" in error)) {
+        this.status = TaskStatus.FAILED;
+        thread.isDead = true;
+        this.resolvers.reject(error);
       }
     };
   }
